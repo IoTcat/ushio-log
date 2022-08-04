b95b87e852cd9ae5b7ed85a47269f4b9f06e917f
iotcat <i@iotcat.me>
2022-08-04 08:17:53 +0800

    fix unreachable after mv

--- iis.php
+++ iis.php
@@ -36,7 +36,23 @@ $sid=substr(md5(time()*rand()), 0, 16);
 
 ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; 4399Box.560; .NET4.0C; .NET4.0E)');
 
-$ipData = file_get_contents('http://ip-api.com/json/49.64.195.96');
+//$ipData = file_get_contents('http://ip-api.com/json/49.64.195.96');
+$ipData = '{
+    "status": "success",
+    "country": "China",
+    "countryCode": "CN",
+    "region": "JS",
+    "regionName": "Jiangsu",
+    "city": "Suzhou",
+    "zip": "",
+    "lat": 31.3093,
+    "lon": 120.602,
+    "timezone": "Asia/Shanghai",
+    "isp": "Chinanet",
+    "org": "Chinanet JS",
+    "as": "AS4134 CHINANET-BACKBONE",
+    "query": "49.64.195.96"
+}';
 $ipData = json_decode($ipData,true);
 
 yimian__log("log_iis",array("sid"=>$sid,"country"=>$ipData['country'],"region"=>$ipData['regionName'],"city"=>$ipData['city'],"isp"=>$ipData['isp'],"address"=>$ipData['as'],"org"=>$ipData['org'],"latitude"=>$ipData['lat'],"longitude"=>$ipData['lon'],"fp"=>$fp,"ip"=>ip2long($ip),"domain"=>get_from_domain(),"url"=>$from,"timestamp"=>date('Y-m-d H:i:s', time()),"language"=>$lang,"timezone"=>$timezone,"platform"=>$platform,"ram"=>$ram,"screen"=>$screen,"useragent"=>$userAgent,"webgl"=>$webgl));
